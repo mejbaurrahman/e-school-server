54465325acacad4785f6430be96fa5294994a175
unknown <mejbacsedu@gmail.com>
2022-10-26 19:33:59 +0600

    courses-send

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = 5000
+
+app.use(cors());
+
+const categories = require('./data/categoriesData.json');
+const courses = require('./data/coursesData.json');
+
+app.get('/courses/:id', (req, res)=>{
+    const categoryId = req.params;
+  if(parseInt(categoryId.id)==6){
+    res.send(courses);
+  }else{
+    const courseByCategory = courses.filter(n=>n.category == parseInt(categoryId.id));
+    // console.log(categoryNews);
+    res.send(courseByCategory);
+  }
+  
+})
+
+app.get('/categories', (req, res)=>{
+    res.json(categories);
+})
+app.get('/courses', (req, res)=>{
+    res.json(courses);
+})
+
+app.get('')
+app.get('/', (req, res) => {
+    res.send('Welcome to E -school');
+  })
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
