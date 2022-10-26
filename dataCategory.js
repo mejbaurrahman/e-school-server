cbf9bad28c55be3d72b029dd5d80bce36124f9f4
unknown <mejbacsedu@gmail.com>
2022-10-26 18:15:19 +0600

    data-create

---
+++ dataCategory.js
@@ -0,0 +1,23 @@
+const b=[
+    {
+        id:1,
+        name: 'Programming'
+    },
+    {
+        id:2,
+        name: 'Web Development'
+    },
+    {
+        id:3,
+        name: 'Digital Marketing'
+    },
+    {
+        id:4,
+        name: 'Graphics Design'
+    },
+    {
+        id:5,
+        name: 'Freelancing'
+    },
+    
+]
